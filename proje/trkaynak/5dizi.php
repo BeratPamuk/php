e56f0c351496bfb9a44b318bd814e023dc854193
Berat Pamuk <beratpamuk2868@gmail.com>
2024-08-08 13:00:39 +0300

    arrays

---
+++ proje/trkaynak/5dizi.php
@@ -0,0 +1,9 @@
+<?php
+
+    $renkler = array("kırmızı","sarı","mavi");
+
+    
+    echo $renkler[0]."<br>";
+    echo $renkler[1]."<br>";
+    echo $renkler[2]."<br>";
+?>
--- proje/uygulama-2/index.php
+++ proje/uygulama-2/index.php
@@ -1,6 +1,8 @@
 <?php
 
     //kurs 1
+const tittle = "Popüler olan kurslar";
+
     $kurs1_baslik = "Php Kursu";
     $kurs1_altBaslik = "Sıfırdan ileri seviye Php ile web geliştirme";
     $kurs1_resim = "php.jpg";
@@ -29,6 +31,12 @@
     $kurs3_altBaslik = ucfirst(strtolower($kurs3_altBaslik));
 
     $kurs1_altBaslik = substr($kurs1_altBaslik,0,35)."...";
+    $kurs2_altBaslik = substr($kurs2_altBaslik,0,35)."...";
+    $kurs3_altBaslik = substr($kurs3_altBaslik,0,35)."...";
+
+    $kurs1_url = str_replace([" "],["-"],strtolower($kurs1_altBaslik));
+    $kurs2_url = str_replace([" "],["-"],strtolower($kurs2_altBaslik));
+    $kurs3_url = str_replace([" "],["-"],strtolower($kurs3_altBaslik));
 
 ?>
 
@@ -43,45 +51,20 @@
     <title>Kurs</title>
 </head>
 <body>
-
-<nav class="navbar navbar-expand-lg bg-body-tertiary">
+  
+<nav class="navbar bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto ml-2 mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-      <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success" type="submit">Search</button>
-      </form>
-    </div>
+    <form class="d-flex" role="search">
+      <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success" type="submit">Search</button>
+    </form>
   </div>
 </nav>
 
+
+<!-- -------------------------------------------------------------------------------------- -->
     <div class="container my-5">
+      <h1 class="mb-3"><?php echo tittle; ?></h1>
         <!-- php card -->
         <div class="card mb-5">
             <div class="row">
@@ -90,7 +73,11 @@
                 </div>
                 <div class="col-9">
                     <div class="card-body">
-                        <h5 class="card-tittle"><?php echo $kurs1_baslik;?></h5>
+                        <h5 class="card-tittle">
+                          <a href="<?php echo $kurs1_url; ?>">
+                          <?php echo $kurs1_baslik;?>
+                          </a>
+                      </h5>
                         <p class="card-text"><?php echo $kurs1_altBaslik;?></p>
                         <p>
                             <span class="badge rounded-pill text-bg-primary">
@@ -102,7 +89,7 @@
                             </span>
 
                             <span class="badge rounded-pill text-bg-secondary">
-                               Tarih: <?php echo $kurs3_yayinTarih;?> 
+                               Tarih: <?php echo $kurs1_yayinTarih;?> 
                             </span>
 
                             <button type="button" class="btn btn-primary position-absolute top-0 end-0 translate-middle-y">
@@ -122,7 +109,11 @@
                 </div>
                 <div class="col-9">
                     <div class="card-body">
-                        <h5 class="card-tittle"><?php echo $kurs2_baslik;?></h5>
+                        <h5 class="card-tittle">
+                          <a href="<?php echo $kurs2_url; ?>">
+                          <?php echo $kurs2_baslik;?>
+                          </a>
+                      </h5>
                         <p class="card-text"><?php echo $kurs2_altBaslik;?></p>
                         <p>
                             <span class="badge rounded-pill text-bg-primary">
@@ -154,7 +145,11 @@
                 </div>
                 <div class="col-9">
                     <div class="card-body">
-                        <h5 class="card-tittle"><?php echo $kurs3_baslik;?></h5>
+                        <h5 class="card-tittle">
+                          <a href="<?php echo $kur3_url; ?>">
+                          <?php echo $kurs3_baslik;?>
+                          </a>
+                      </h5>
                         <p class="card-text"><?php echo $kurs3_altBaslik;?></p>
                         <p>
                             <span class="badge rounded-pill text-bg-primary">
@@ -177,8 +172,6 @@
                 </div>
             </div>
         </div>
-
-    </div>
 
 </body>
 </html>
